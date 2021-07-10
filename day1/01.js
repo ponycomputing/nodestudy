d6b6cce99efead630c898c6045d78b19c44e4626
Pony Computing Limited <74728427+ponycomputing@users.noreply.github.com>
2021-07-11 07:12:53 +1200

    Update 01.js
    create test.txt file, write a script in this 01.js file, to see non blocking system

--- day1/01.js
+++ day1/01.js
@@ -1,1 +1,17 @@
+//NodeJS non blocking I/) system
 
+//built-in module，file system
+var fs = require("fs");
+
+//reading files
+fs.readFile("./test.txt",function(err,data){
+	//callback function，doing things after reading files
+	console.log(data.toString());
+});
+
+//do sum calculation
+var sum = 0;
+for(var i = 0 ; i <= 100 ; i++){
+	sum += i;
+}
+console.log(sum);
