3041ea7ed4b37ecbd85af98cb3b2398b0add5220
Pony Computing Limited <74728427+ponycomputing@users.noreply.github.com>
2021-07-13 05:44:41 +1200

    Update 01readfile.js

--- day1/01readfile.js
+++ day1/01readfile.js
@@ -5,14 +5,14 @@ var fs = require("fs");
 
 //reading files
 fs.readFile("./test.txt",function(err,data){
-	//callback function，doing things after reading files
-	console.log(data.toString());
+    //callback function，doing things after reading files
+    console.log(data.toString());
 });
 
 //do sum calculation
 var sum = 0;
 for(var i = 0 ; i <= 100 ; i++){
-	sum += i;
+    sum += i;
 }
 console.log(sum);
 
