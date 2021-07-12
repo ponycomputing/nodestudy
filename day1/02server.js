d579d861673e4ab7ec5c0cf1debc51a08382e4a6
Pony Computing Limited <74728427+ponycomputing@users.noreply.github.com>
2021-07-13 05:48:56 +1200

    Create 02server.js

---
+++ day1/02server.js
@@ -0,0 +1,13 @@
+//Hello World
+
+//using built-in moudule, for create server
+var http = require("http");
+
+//create server
+var server = http.createServer(function(req,res){
+	res.end("<h1>One week has" + (2 + 5) + "days！</h1>");
+});
+
+//listen
+server.listen(3000);
+console.log("server is running at port 3000");
