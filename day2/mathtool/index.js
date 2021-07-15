d74de1125d6101c24a62823a5725a42204285584
Pony Computing Limited <74728427+ponycomputing@users.noreply.github.com>
2021-07-16 04:24:52 +1200

    Create index.js
    connect day2 01.js

---
+++ day2/mathtool/index.js
@@ -0,0 +1,12 @@
+var base = require("./base.js");
+var circle = require("./circle.js");
+
+
+exports.sum = base.sum;
+exports.average = base.average;
+exports.powerof = base.powerof;
+exports.maxnumber = base.maxnumber;
+
+
+exports.sqaure = circle.sqaure;
+exports.perimeter = circle.perimeter;
