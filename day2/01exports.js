09a190731bcc7e25a342c337c7a126d41e765b53
Pony Computing Limited <74728427+ponycomputing@users.noreply.github.com>
2021-07-16 04:18:45 +1200

    Update and rename 01.js to 01exports.js

---
+++ day2/01exports.js
@@ -0,0 +1,9 @@
+var mt = require("./mathtool");
+
+
+console.log(mt.sum(50,60,70,80));
+console.log(mt.average(50,60,70,80));
+console.log(mt.powerof(50));
+console.log(mt.maxnumber(50,60,70,80));
+
+console.log(mt.sqaure(100000000));
